ced67257d052c22f50e692749144bee30c102a09
George5555ish <mbajiakugeorge@yahoo.com>
2021-01-25 22:05:31 +0100

    git and backend changes

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL:'http://localhost:4000'});
+const API = axios.create({baseURL:'https://mern-memories-backend.zeet.app'});
 
 API.interceptors.request.use((req) => {
      
